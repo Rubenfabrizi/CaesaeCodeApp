311e90c849c2a950ba30f1fa9157eeb10c4b8cf5
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-11-20 10:55:55 +0100

    Add project files.

---
+++ CaesaeCodeApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CaesaeCodeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ciao"); 
+        }
+    }
+}
